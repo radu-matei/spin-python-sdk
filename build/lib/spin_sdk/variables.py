266c538b2db9657091223cc742f233aef7f0e4a2
Radu Matei <radu@fermyon.com>
2026-03-09 16:53:09 +0100

    chore: gitignore build directory
    Signed-off-by: Radu Matei <radu@fermyon.com>

--- build/lib/spin_sdk/variables.py
+++
@@ -1,9 +0,0 @@
-"""Module for interacting with Spin Variables"""
-
-from spin_sdk.wit.imports import variables
-
-def get(key: str):
-    """
-    Gets the value of the given key
-    """
-    return variables.get(key)
